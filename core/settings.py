b30ed265174a385d409c51680147bc7cbaba2651
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-07-23 00:25:27 +0300

    Create model Question in DB

--- core/settings.py
+++ core/settings.py
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk'
 
-TIME_ZONE = 'DST'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,21 @@
-from django.db import models
+import datetime
+
+from django.db import models # type: ignore
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
-    qustion_text = models.CharField(max_length = 200)
+    question_text = models.CharField(max_length = 200)
     publish_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
