8d68d6cf3576fe36cc2eb3c536b9d419056d1d51
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-08-02 22:21:53 +0300

    nothing

--- Solo_Leveling/views.py
+++ Solo_Leveling/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.urls import reverse
 from django.contrib import messages
 from Solo_Leveling.func import validate_url
