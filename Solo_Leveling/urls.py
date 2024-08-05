3e4454b7d1a9ac7db422393aff000f5a959a199b
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-08-05 23:45:17 +0300

    Adding pagination of pages on the site

--- Solo_Leveling/urls.py
+++ Solo_Leveling/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = "SL"
 
 urlpatterns = [
-    path('', views.common, name='common'),
+    path('page=<int:page_number>/', views.common, name='common'),
     path('ep/', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('create_post/', views.create_post, name='create_post'),
