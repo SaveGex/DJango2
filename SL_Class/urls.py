54626bfb569241a9cc82fa4c49d11a1a28606b60
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-08-11 18:09:17 +0300

    I have created functions (Main, Index, Create)

--- SL_Class/urls.py
+++ SL_Class/urls.py
@@ -6,6 +6,9 @@ from . import views
 app_name = "SL_Class"
 
 urlpatterns = [
+    path('page=<int:page_number>/', views.Main.as_view(), name='common'),
+    path('ep/', views.Index.as_view(), name='index'),
+    path('create_post/', views.Create.as_view(), name='create_post')
     # path('page=<int:page_number>/', views.common, name='common'),
     # path('ep/', views.index, name='index'),
     # path('create_post/', views.create_post, name='create_post'),
