a1c81864a89974d37012a2c73d3ec1123da7ba50
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-08-15 18:20:10 +0300

    All views in SL_Class to working

--- SL_Class/forms.py
+++ SL_Class/forms.py
@@ -3,13 +3,17 @@ from django.utils.translation import gettext_lazy as _
 from Solo_Leveling.models import GVideo
 
 class Create(forms.ModelForm):
-
     class Meta:
         model = GVideo
         exclude = ['publish_date']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control form-label'}),
+            'comment_video': forms.Textarea(attrs={'class': 'form-control form-label', 'rows': 3}),
+            'url_video': forms.TextInput(attrs={'class': 'form-control form-label'}),
+        }
         error_messages = {
-            "name": {
-                "max_length": _("This writer's name is too long."),
+            'name': {
+                'max_length': _("This writer's name is too long."),
             },
         }
 
--- SL_Class/urls.py
+++ SL_Class/urls.py
@@ -6,9 +6,12 @@ from . import views
 app_name = "SL_Class"
 
 urlpatterns = [
-    path('page=<int:page_number>/', views.Main.as_view(), name='common'),
-    path('ep/', views.Index.as_view(), name='index'),
-    path('create_post/', views.Create.as_view(), name='create_post')
+    path('page=<int:page_number>/', views.MainView.as_view(), name='common'),
+    path('ep/', views.IndexView.as_view(), name='index'),
+    path('create_post/', views.CreateCView.as_view(), name='create_post'),
+    path('about=<int:pk>', views.AboutView.as_view(), name='about'),
+    path('delete=<int:pk>', views.DeleteCView.as_view(), name='delete'),
+    path('change=<int:pk>', views.ChangeView.as_view(), name='change'),
     # path('page=<int:page_number>/', views.common, name='common'),
     # path('ep/', views.index, name='index'),
     # path('create_post/', views.create_post, name='create_post'),
